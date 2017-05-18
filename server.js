77193c9faaaf459c7903f0ef87a567c8627e94af
SaiSujitha29 <venkatesan.sa@husky.neu.edu>
2017-05-18 15:36:49 -0400

    change in server.js

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-var app = require('./express');
+var express = require('express');
+var app = express();
 
-app.set('view engine', 'ejs');
-require('./utilities/filelist');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(app.express.static(__dirname + '/public'));
+// configure a public directory to host static content
+app.use(express.static(__dirname + '/public'));
 
-app.listen(process.env.PORT || 3000);
+require ("./test/app.js")(app);
+
+var port = process.env.PORT || 3000;
+
+app.listen(port);
