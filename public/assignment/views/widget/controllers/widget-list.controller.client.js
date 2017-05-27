1b0e7a87f457564e9804e78d50d34e2f6b598c2c
SaiSujitha29 <venkatesan.sa@husky.neu.edu>
2017-05-27 17:29:49 -0400

    changing ng-switch

--- public/assignment/views/widget/controllers/widget-list.controller.client.js
+++ public/assignment/views/widget/controllers/widget-list.controller.client.js
@@ -3,9 +3,13 @@
         .module('WAM')
         .controller('widgetListController', widgetListController);
     
-    function widgetListController($sce) {
+    function widgetListController($sce, $routeParams) {
 
         var model = this;
+        model.user_id = $routeParams['userId'];
+        model.websiteId = $routeParams.websiteId;
+        model.pageId = $routeParams.pageId;
+        model.widgetId = $routeParams.widgetId;
 
         var widgets = [
             { "_id": "123", "widgetType": "HEADING", "pageId": "321", "size": 2, "text": "GIZMODO"},
@@ -21,7 +25,8 @@
 
         model.widgets = widgets;
         model.trustThisContent = trustThisContent;
-        model.getYouTubeEmbedUrl = getYouTubeEmbedUrl
+        model.getYouTubeEmbedUrl = getYouTubeEmbedUrl;
+        model.getWidgetUrlForType = getWidgetUrlForType;
 
         function trustThisContent(html) {
             //diligence to scrub any unsafe content
@@ -34,6 +39,10 @@
             var id = youTubeLinkParts[youTubeLinkParts.length - 1];
             embedUrl += id;
             return $sce.trustAsResourceUrl(embedUrl);
+        }
+
+        function getWidgetUrlForType(type) {
+            return 'views/widget/templates/widget-'+ type.toLowerCase()+'.view.client.html';
         }
 
     }
