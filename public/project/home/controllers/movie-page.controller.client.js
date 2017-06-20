89a210e88f751a9f02bbf9dab1c4a3c547e1053d
SaiSujitha29 <venkatesan.sa@husky.neu.edu>
2017-06-20 15:43:00 -0400

    problem with trailers

--- public/project/home/controllers/movie-page.controller.client.js
+++ public/project/home/controllers/movie-page.controller.client.js
@@ -3,7 +3,7 @@
         .module('MovieApp')
         .controller('movieController', movieController);
 
-    function movieController($location, $routeParams, homeService) {
+    function movieController($sce, $location, $routeParams, homeService) {
         var model = this;
         model.movieId = $routeParams['movieId'];
 
@@ -12,10 +12,57 @@
                 .searchMovieById(model.movieId)
                 .then(function (response) {
                     model.movie = response.data;
-                    console.log(model.movie);
+                    model.genres = response.data.genres;
+                    var path = model.movie.backdrop_path;
+                    //model.path = response.data.backdrop_path;
+                    //document.body.style.background = 'url(' + 'http://image.tmdb.org/t/p/original' + model.path + ') no-repeat top left';
+                    //document.body.style.backgroundSize = 1;
                 });
+
+            homeService
+                .searchCast(model.movieId)
+                .then(function (response) {
+                    model.casts = response.data.cast;
+                });
+
+            homeService
+                .similarMovies(model.movieId)
+                .then(function (response) {
+                    var data = response.data.results;
+                    model.similarMovie = [];
+                    for(i =0; i < data.length; i++){
+                        model.similarMovie.push(data[i]);
+                    }
+                });
+
+            homeService
+                .recommendedMovies(model.movieId)
+                .then(function (response) {
+                    var data = response.data.results;
+                    model.recommendedMovie = [];
+                    for(i =0; i < data.length; i++){
+                        model.recommendedMovie.push(data[i]);
+                    }
+                })
         }
         init();
+
+        model.getYouTubeEmbedUrl = getYouTubeEmbedUrl;
+        model.selectMovie = selectMovie;
+
+        function getYouTubeEmbedUrl() {
+            return homeService
+                .searchVideos(model.movieId)
+                .then(function (response) {
+                    model.videoKeys = response.data.results;
+                })
+        }
+
+        function selectMovie(movieId) {
+            $location.url('/page/' + movieId);
+        }
+
+
     }
 
 })();
