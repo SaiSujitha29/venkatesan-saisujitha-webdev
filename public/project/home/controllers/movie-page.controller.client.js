93ab108ccc03e39ffe3548fa97b20d5d8031960b
SaiSujitha29 <venkatesan.sa@husky.neu.edu>
2017-06-26 13:14:20 -0400

    form for posts

--- public/project/home/controllers/movie-page.controller.client.js
+++ public/project/home/controllers/movie-page.controller.client.js
@@ -168,6 +168,16 @@
             $location.url('/user/'+ userId + '/profile-public');
         }
 
+        $(document).on('change', '.div-toggle', function() {
+            var target = $(this).data('target');
+            var show = $("option:selected", this).data('show');
+            $(target).children().addClass('hide');
+            $(show).removeClass('hide');
+        });
+        $(document).ready(function(){
+            $('.div-toggle').trigger('change');
+        });
+
     }
 
 })();
